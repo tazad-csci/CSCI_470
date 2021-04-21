1a04a52a04df57fd8e5a6dcbf64c6340c50b606d
tazad-csci <63663893+tazad-csci@users.noreply.github.com>
2021-04-20 23:14:20 -0500

    Update GraphicsFrame.java

--- Assign6/GraphicsFrame.java
+++ Assign6/GraphicsFrame.java
@@ -1,12 +1,12 @@
 /***************************************************************
-CSCI 470         Program 4     Spring 2021
+CSCI 470         Program 6     Spring 2021
 
 Programmer: Tamjid Azad
 
 Date Due: 4/23/21
 
 Purpose: Created GraphicsFrame which contains the two frames to
-		     run our program.
+	 run our program.
 ***************************************************************/
 package package1;
 
@@ -29,27 +29,36 @@ public class GraphicsFrame extends JFrame{
       frame.setVisible(true); 
    }
 
+	/***************************************************************
+	 Function: GraphicsFrame()
+	 
+	 Use: GraphicsFrame Constructor
+
+	 Arguments: None
+
+	 Returns: Nothing
+  ***************************************************************/
    public GraphicsFrame() { //constructor
 	 super();
 	 //final Color VERY_LIGHT_GARY = new Color(204,204,204);
 	 
-	  JPanel panelOne = new JPanel();
-    JPanel panelTwo = new JPanel();
+	 JPanel panelOne = new JPanel();
+     JPanel panelTwo = new JPanel();
      
-    panelOne.setLayout(new BorderLayout());
-    panelTwo.setLayout(new BorderLayout());
+     panelOne.setLayout(new BorderLayout());
+     panelTwo.setLayout(new BorderLayout());
      
-    panelOne.setPreferredSize(new Dimension(638, 300));
-    panelTwo.setPreferredSize(new Dimension(638, 300));
+     panelOne.setPreferredSize(new Dimension(638, 300));
+     panelTwo.setPreferredSize(new Dimension(638, 300));
      
-    this.add(panelOne,BorderLayout.WEST);
-    this.add(panelTwo,BorderLayout.EAST);
+     this.add(panelOne,BorderLayout.WEST);
+     this.add(panelTwo,BorderLayout.EAST);
      
-	  MainPanel mainPanelPtr = new MainPanel(); //create mainpanel that will overwrites the frame
-	  BallAnimation ballAnimationPtr = new BallAnimation();
+	 MainPanel mainPanelPtr = new MainPanel(); //create mainpanel that will overwrites the frame
+	 BallAnimation ballAnimationPtr = new BallAnimation();
 	 
-	  panelOne.add(mainPanelPtr);
-	  panelTwo.add(ballAnimationPtr);
+	 panelOne.add(mainPanelPtr);
+	 panelTwo.add(ballAnimationPtr);
 	 
    }
 }
