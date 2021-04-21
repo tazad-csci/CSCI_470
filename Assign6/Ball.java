2f475d84ab6a323f56b9b426521de906f87d233e
tazad-csci <63663893+tazad-csci@users.noreply.github.com>
2021-04-20 23:12:52 -0500

    Update Ball.java

--- Assign6/Ball.java
+++ Assign6/Ball.java
@@ -1,5 +1,5 @@
 /***************************************************************
-CSCI 470         Program 4     Spring 2021
+CSCI 470         Program 6     Spring 2021
 
 Programmer: Tamjid Azad
 
@@ -19,6 +19,15 @@ public class Ball  {
 	 int x, y;
 	 int dx, dy;
  
+	 /***************************************************************
+	 Function: Ball(Color col, int rad, int x, int y, int dx, int dy)
+
+	 Use: Ball constructor 
+
+	 Arguments: None
+
+	 Returns: Nothing
+     ***************************************************************/
 	 Ball(Color col, int rad, int x, int y, int dx, int dy) { //Constructor statement.
 		 this.color = col;
 		 this.radius = rad;
@@ -28,6 +37,17 @@ public class Ball  {
 		 this.dy = dy;
 	 }
 
+	 
+	/***************************************************************
+	 Function: void move(Dimension dPanel)
+
+	 Use: Calculates how the call should moce based on its measurements.
+
+	 Arguments: A dimension object that contains the width & height 
+	 		    of the panel in which the ball is moving.
+
+	 Returns: Nothing
+     ***************************************************************/
 	 public void move(Dimension dPanel) { //Move function.
 		 if(x <= radius || x >= (dPanel.width - ( 2 * radius))) //Checks to see if ball has hit the edge of the panel.
 		 dx = -dx; //If so, switch direction.
@@ -40,6 +60,18 @@ public class Ball  {
 		 
 	 }
 
+	 
+	/***************************************************************
+	 Function: void draw(Graphics g) 
+
+	 Use: Use the object to set the drawing color to the color of the
+	 	  ball and then use it to call fillOval() to draw the ball.
+
+	 Arguments: Graphics object that will beused to draw the ball on
+	 		    an off-screen image.
+
+	 Returns: Nothing
+     ***************************************************************/
 	 public void draw(Graphics g) //Draws the ball in its new location, chaning x and y each time.
 	 {
 		 g.setColor(color); //Sets the color to whatever the balls color was.
@@ -47,4 +79,3 @@ public class Ball  {
 	 }
 	 	 
 }
-
