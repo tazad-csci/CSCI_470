39128b9f2c05f86a574da06be766e2375d9aa328
tazad-csci <63663893+tazad-csci@users.noreply.github.com>
2021-04-20 22:49:31 -0500

    Create GraphicsFrame.java

---
+++ Assign6/GraphicsFrame.java
@@ -0,0 +1,55 @@
+/***************************************************************
+CSCI 470         Program 4     Spring 2021
+
+Programmer: Tamjid Azad
+
+Date Due: 4/23/21
+
+Purpose: Created GraphicsFrame which contains the two frames to
+		     run our program.
+***************************************************************/
+package package1;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.*;
+
+public class GraphicsFrame extends JFrame{
+   public static void main(String[] args) {
+     
+	   
+      GraphicsFrame frame = new GraphicsFrame(); //create our frame and constructor puts large //main panel over the entire frame
+     
+      //set the name of the frame
+      frame.setName("Frame");
+     
+      //set the size of the frame
+      frame.setSize(400, 400);    
+      frame.setVisible(true); 
+   }
+
+   public GraphicsFrame() { //constructor
+	 super();
+	 //final Color VERY_LIGHT_GARY = new Color(204,204,204);
+	 
+	  JPanel panelOne = new JPanel();
+    JPanel panelTwo = new JPanel();
+     
+    panelOne.setLayout(new BorderLayout());
+    panelTwo.setLayout(new BorderLayout());
+     
+    panelOne.setPreferredSize(new Dimension(638, 300));
+    panelTwo.setPreferredSize(new Dimension(638, 300));
+     
+    this.add(panelOne,BorderLayout.WEST);
+    this.add(panelTwo,BorderLayout.EAST);
+     
+	  MainPanel mainPanelPtr = new MainPanel(); //create mainpanel that will overwrites the frame
+	  BallAnimation ballAnimationPtr = new BallAnimation();
+	 
+	  panelOne.add(mainPanelPtr);
+	  panelTwo.add(ballAnimationPtr);
+	 
+   }
+}
